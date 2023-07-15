8af0dd1a5f4ea3c4084257d7a4ad7fc10e7565fe
lazebird <lazebird@163.com>
2023-07-15 05:10:11 +0000

    Deploying to gh-pages from @ lazebird/jtools@02c55e95dcd6dae79616777179566655f83b696d 🚀

---
+++ assets/index.06d6bb27.js
@@ -0,0 +1,23 @@
+import{a as ref,L as computed,o as openBlock,b as createElementBlock,f as createVNode,i as unref,d as createBaseVNode}from"./vendor.58b05717.js";import inputVue from"./input.f3d5fe79.js";import codeVue from"./code.5c6ba1f9.js";import outputVue from"./output.b960d5cb.js";/* empty css               */import"./index.35c8cff3.js";const _hoisted_1={style:{margin:"20px"}},_hoisted_2=createBaseVNode("br",null,null,-1),_hoisted_3=createBaseVNode("br",null,null,-1),_sfc_main={setup(__props){const input=ref({name:"Input",content:""}),options=[{name:"prettier",value:"(n) => { const o = JSON.parse(n); return JSON.stringify(o, null, 4); }"},{name:"po2cvs",value:`
+        (str) => {
+          const lines = str.replace('\\r', '').split('\\n').filter(l=>l.length);
+          if (lines.length & 1) return "length error: "+ lines.length;
+          const chunks = [];
+          for (var i = 0, len = lines.length; i < len; i += 2) chunks.push(lines.slice(i, i+2));
+          const obj = {};
+          for (var [id, str] of chunks) obj[id.replace('msgid', '').trim()] = str.replace('msgstr', '').trim();
+          var result = '';
+          for (const k of Object.keys(obj).sort()) result += k + ',' + obj[k] + '\\r\\n';
+          return result;
+        }
+      `},{name:"cvs2po",value:`
+        (str) => {
+          const lines = str.replace('\\r', '').split('\\n').filter(l=>l.length);
+          const chunks = lines.map(l=>l.split(','));
+          const obj = {};
+          for (var [id, str] of chunks) obj[id.trim()] = str.trim();
+          var result = '';
+          for (const k of Object.keys(obj).sort()) result += 'msgid ' + k + '\\r\\n' + 'msgstr ' + obj[k] + '\\r\\n\\r\\n';
+          return result;
+        }
+        `}],code=ref({name:"Code",options,content:options[0].value}),output=computed(()=>{try{const parser=eval(code.value.content);return typeof parser=="function"?{content:parser(input.value.content)}:{content:"code is not a valid function!",class:"error"}}catch(r){return{content:r.toString(),class:"error"}}});return(r,t)=>(openBlock(),createElementBlock("div",_hoisted_1,[createVNode(inputVue,{data:input.value,"onUpdate:data":t[0]||(t[0]=e=>input.value=e)},null,8,["data"]),_hoisted_2,createVNode(codeVue,{data:code.value,"onUpdate:data":t[1]||(t[1]=e=>code.value=e)},null,8,["data"]),_hoisted_3,createVNode(outputVue,{data:unref(output)},null,8,["data"])]))}};export{_sfc_main as default};
